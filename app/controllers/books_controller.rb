73596386d73fa24d4efe1665adf90917e9dd9eb1
nick stern <nicholas.stern2@gmail.com>
2019-09-22 18:12:38 +0800

    book controller index,show,new,create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,19 @@
 class BooksController < ApplicationController
-   def index
+  def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.save!
   end
 
   def edit
@@ -18,5 +23,11 @@ class BooksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def book_params
+    params.permit(:book).require(:title)
   end
 end
